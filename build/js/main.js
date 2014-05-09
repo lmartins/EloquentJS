067cde338c1b89861d870a6a1c0129e01ad2f102
Luis Martins <luis.martins@multiweb.pt>
2014-05-09 19:21:10 +0100

    Chapter 8 ongoing

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,22 @@
 'use strict';
+var Rabbit, killerRabbit;
 
+Rabbit = (function() {
+  function Rabbit(adjective) {
+    this.adjective = adjective;
+  }
+
+  Rabbit.prototype.speak = function(line) {
+    return console.log("The " + this.adjective + " rabbit says '" + line + "'");
+  };
+
+  return Rabbit;
+
+})();
+
+killerRabbit = new Rabbit('killer');
+
+killerRabbit.speak('GRAAAAAA!');
 
 /*
 //# sourceMappingURL=main.js.map
